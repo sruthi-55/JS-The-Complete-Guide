aee9b5d610eab41c81ccf8e2feacaac40679ff2d
Sruthi Mora <sruthimora02@gmail.com>
2024-12-26 22:40:15 +0530

    started section 2

---
+++ resources/S02/practice/basics-01-starting-project/assets/scripts/basics.js
@@ -0,0 +1,21 @@
+//variables and constants
+let myNickname = "Sru";
+myNickname = "Sruu";
+const myName = "Sruthi Mora";
+
+//camelCase standard - naming convention
+//allowed variable names
+let $rupee;
+let _myval;
+let age2;
+let user_name;
+// let 2player;
+
+//Data types
+//Number, String(',",`)
+
+let val = `(${defRes}+10*3)`; //template literal
+
+let wSpace = `1
+                2`;
+//set "white-space: pre" in css style
